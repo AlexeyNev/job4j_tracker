5a4fae1753b44f8ffc926bbc8d478f9b5876a346
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-05-24 14:55:41 +0400

    [#173889 #304191]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -2,15 +2,24 @@ package ru.job4j.oop;
 
 public class Error {
 
+    private boolean active;
+    private int status;
+    private String message;
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
+
     }
 
-    private boolean active;
-    private int status;
-    private String message;
+    public Error() {
+
+    }
+
+    public void printInfo2() {
+        System.out.println("Error");
+    }
 
     public void printInfo() {
         System.out.println("Error: " + active);
@@ -20,6 +29,8 @@ public class Error {
     }
 
     public static void main(String[] args) {
+        Error rsl = new Error();
+        rsl.printInfo2();
         Error error = new Error(true, 403, "Все ок");
         error.printInfo();
         Error err = new Error(false, 404, "Не ок");
