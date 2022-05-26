8303447027fc2d467b0c9b220c2a922b0e098d64
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-05-26 15:00:51 +0400

    [#122642 #304991]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -14,24 +14,24 @@ public class TriangleTest {
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
-        assertThat(rsl, closeTo(-1, 0.001));
+        assertThat(rsl, closeTo(8, 0.001));
     }
 
     @Test
-    public void when00and60and02Then() {
+    public void when00and20and02Then2() {
         Point a = new Point(0, 0);
-        Point b = new Point(6, 0);
+        Point b = new Point(2, 0);
         Point c = new Point(0, 2);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
-        assertThat(rsl, closeTo(-1, 0.001));
+        assertThat(rsl, closeTo(2, 0.001));
     }
 
     @Test
-    public void when10and50and03Then() {
-        Point a = new Point(1, 0);
-        Point b = new Point(5, 0);
-        Point c = new Point(0, 3);
+    public void whenTriangleNotExist() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 0);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(-1, 0.001));
