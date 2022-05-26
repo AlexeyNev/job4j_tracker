f4b2d67cacaaf6c033336da8c229d654a7e514ed
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-05-26 12:22:00 +0400

    [#122642 #304991]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -16,4 +16,24 @@ public class TriangleTest {
         double rsl = triangle.area();
         assertThat(rsl, closeTo(-1, 0.001));
     }
+
+    @Test
+    public void when00and60and02Then() {
+        Point a = new Point(0, 0);
+        Point b = new Point(6, 0);
+        Point c = new Point(0, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
+    }
+
+    @Test
+    public void when10and50and03Then() {
+        Point a = new Point(1, 0);
+        Point b = new Point(5, 0);
+        Point c = new Point(0, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
+    }
 }
