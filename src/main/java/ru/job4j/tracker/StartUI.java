2d413d00d4730ed2a5bc4707a6601395eb829c1d
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-06-06 14:13:42 +0400

    [#310018 #309529]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,17 +1,9 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    @Override
-    public String toString() {
-        return "StartUI{}";
-    }
 
     public static void main(String[] args) {
         Item item = new Item(1, "User1");
-        System.out.println("User: "
-                + item.getId() + " "
-                + item.getName() + " "
-                + item.getCreated()
-        );
+        System.out.println(item);
     }
 }
