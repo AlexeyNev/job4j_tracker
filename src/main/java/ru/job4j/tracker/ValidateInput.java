1c055ca5a30c12fdafd441ba700397d29eac8a21
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-07-11 19:05:03 +0400

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,6 +1,24 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+/**
+ *     Если нужно выбрать - как организовать проверку с помощью if() или использовать блок catch с исключением
+ *  * - то выбирать стоит второе. Поскольку такой вариант работает быстрее
+ *  * и нам не требуется писать код для дополнительной проверки
+ */
+public class ValidateInput implements Input {
+    private final Output out;
+    private final Input in;
+    private ValidateInput input;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
@@ -8,10 +26,10 @@ public class ValidateInput extends ConsoleInput {
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
-            } catch (NumberFormatException nfe) {
-                System.err.println("Please enter validate data again.");
+            } catch (NumberFormatException rsl) {
+                System.out.println("Please enter validate data again ");
             }
         } while (invalid);
         return value;
