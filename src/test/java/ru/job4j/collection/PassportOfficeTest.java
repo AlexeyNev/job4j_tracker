985b3db7ebe479a631a38120556c8818eedcd2ac
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-14 16:10:35 +0400

    1. Паспорт и жители [#212656 #342343]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -21,6 +21,6 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen1);
         office.add(citizen2);
-        assertFalse(citizen1.getPassport(), false);
+        assertThat(citizen1.getPassport()).isFalse();
     }
 }
