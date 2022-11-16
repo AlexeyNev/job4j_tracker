df12ba9b4078c5a45c6089c2bcf4008eacccd5a3
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-16 13:28:54 +0400

    3. Отладка лямбда в IDEA [#259165]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+/**
+ * 1. Произведите рефакторинг кода по принципу строка занимает 80 символов.
+ * 2. Проанализируйте следующий код. Найдите в нем ошибку. Программа должна выводить задачи имя которых "Bug" и значение spent > 30.
+ */
+
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30).map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
+    }
+}
