3699cbb87a299bee983426a8f12bf0b9ca8ec0bd
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-20 18:58:45 +0400

    4. Перенос методов в Stream API [#279131 #361957]

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -3,13 +3,14 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//public class ProductLabel {
-//    public List<String> generateLabels(List<Product> products) {
-//        return products.stream()
-//                .filter(e -> (e.getStandard() - e.getActual() >= 0 && (e.getStandard() - e.getActual() < 4)))
-//                .collect(Collectors.toList()));
-//
-//
-//    }
-//}
+public class ProductLabel {
+    public List<String> generateLabels(List<Product> products) {
+        return products.stream()
+                .filter(e -> e.getStandard() - e.getActual() >= 0)
+                .filter(e -> e.getStandard() - e.getActual() <= 3)
+                .map(p -> new Label(p.getName(), p.getPrice()/ 2))
+                .map(p -> p.toString())
+                .collect(Collectors.toList());
+    }
+}
 
