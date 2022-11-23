3a68ea8e90d64028035c24e2bfe6b56da91b5756
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-11-23 13:10:46 +0400

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Необходимо реализовать метод List<Student> levelOf(List<Student> students, int bound);
+ *
+ * Метод должен вернуть список студентов, у которых балл аттестата больше bound.
+ * Во входящем списке могут быть null элементы.
+ * Порядок действий:
+ * - Используя метод filter убрать null;
+ * - Отсортировать список;
+ * - Используя метод takeWhile, получить нужный поток;
+ * - Сохранить поток в List.
+ */
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(Objects::nonNull)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
